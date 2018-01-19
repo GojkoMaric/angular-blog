e58d564d4d014ccc1a37e5868a3ae0b37c37d9a2
GojkoMaric <gojko.maric@yahoo.com>
2018-01-19 18:41:51 +0100

    1st exam->create Blog, bootstrap, Post Component, routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PostsComponent } from './components/posts/posts.component';
 
+const appRoutes: Routes = [
+  {path: '', component: PostsComponent },
+  {path: 'posts', component: PostsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavBarComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
